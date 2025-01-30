bf3dfcfa6b19689f0f7ad65e86811dee3c0af402
Víctor Chagas <victorchagaslm@gmail.com>
2025-01-30 14:36:04 -0300

    feature: Implementando recebimento de dados em chunks

--- handlers/postSensorsDataCaptureHandler.js
+++ handlers/postSensorsDataCaptureHandler.js
@@ -1,15 +1,21 @@
 import { saveSensorsData } from "../utils/sensorsUtils.js";
 import { validateDeviceId } from "../utils/deviceUtils.js";
 
+const chunksStore = {};
+
 export function postSensorsDataCaptureHandler(req, res) {
   const payload = req.payload?.toString();
 
   try {
     const data = JSON.parse(payload);
-    const { deviceId, accelerometerData, gyroscopeData, geolocationData } =
-      data;
+    const { deviceId, chunk, index, totalChunks } = data;
 
-    if (!deviceId || !accelerometerData || !gyroscopeData || !geolocationData) {
+    if (
+      !deviceId ||
+      chunk === undefined ||
+      index === undefined ||
+      totalChunks === undefined
+    ) {
       res.code = "4.00";
 
       return res.end(
@@ -37,36 +43,57 @@ export function postSensorsDataCaptureHandler(req, res) {
       );
     }
 
-    const sensorsData = {
-      accelerometerData,
-      gyroscopeData,
-      geolocationData,
-    };
+    if (!chunksStore[deviceId]) {
+      chunksStore[deviceId] = { totalChunks, chunks: [] };
+    }
 
-    saveSensorsData(deviceId, sensorsData);
+    chunksStore[deviceId].chunks[index] = chunk;
 
-    res.code = "2.05";
-    res.end(
-      JSON.stringify({
-        statusCode: "2.05",
-        body: {
-          message: "Content",
-          data: JSON.stringify({ deviceId, sensorsData }),
-        },
-      })
-    );
+    if (chunksStore[deviceId].chunks.filter(Boolean).length === totalChunks) {
+      const completePayload = chunksStore[deviceId].chunks.join("");
+      let parsedData;
+
+      try {
+        parsedData = JSON.parse(completePayload);
+      } catch (error) {
+        return res.end();
+      }
+
+      delete chunksStore[deviceId];
+
+      const { accelerometerData, gyroscopeData, locationData } = parsedData;
+      const sensorsData = { accelerometerData, gyroscopeData, locationData };
+
+      console.log(JSON.stringify({ deviceId, sensorsData }));
+
+      res.code = "2.05";
+      res.end(
+        JSON.stringify({
+          statusCode: "2.05",
+          body: {
+            message: "Content",
+            data: JSON.stringify({ deviceId }),
+          },
+        })
+      );
+    }
   } catch (error) {
-    console.error("Erro ao processar o payload:", error);
+    if (
+      error.name !== "SyntaxError" ||
+      error.message !== "Unexpected end of JSON input"
+    ) {
+      console.error("Erro ao processar o payload:", error);
 
-    res.code = "4.00";
-    res.end(
-      JSON.stringify({
-        statusCode: "4.00",
-        body: {
-          message: "Bad Request",
-          data: "Erro ao processar o payload.",
-        },
-      })
-    );
+      res.code = "4.00";
+      res.end(
+        JSON.stringify({
+          statusCode: "4.00",
+          body: {
+            message: "Bad Request",
+            data: "Erro ao processar o payload.",
+          },
+        })
+      );
+    }
   }
 }
